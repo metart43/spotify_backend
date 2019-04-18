c2137e8876377740e494c8dfb91364565c8b2713
github username <metelskyi88@gmail.com>
2019-04-18 07:46:23 -0400

    added scopes

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::LoginController < ApplicationController
       client_id: ENV["client_id"],
       response_type: 'code',
       redirect_uri: 'http://localhost:3000/api/v1/auth/spotify/callback',
-      scope: 'user-read-recently-played user-read-playback-state user-library-read user-library-modify user-modify-playback-state playlist-modify-public',
+      scope: 'user-read-recently-played user-read-playback-state user-library-read user-library-modify user-modify-playback-state playlist-modify-public streaming user-read-birthdate user-read-email user-read-private',
       show_dialog: true
     }
     url = 'https://accounts.spotify.com/authorize'
